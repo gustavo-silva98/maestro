fb4eec4f402fa1041e206b3cd4e55c621ce46fc1
Gustavo da Silva Oliveira <silva.gustavo98@gmail.com>
2026-08-06 18:49:37 -0300

    feat: implement buildVolumes function and add tests for volume string generation

--- internal/orchestrator/maestro.go
+++ internal/orchestrator/maestro.go
@@ -41,28 +41,49 @@ func (f *FootballOrchestrator) ExecuteJob(result ExecutionResult) (ExecutionResu
 	if err := f.DB.SetJobPending(ctx, result.Job); err != nil {
 		return ExecutionResult{}, err
 	}
-	cwd, err := os.Getwd()
-	if err != nil {
-		return ExecutionResult{}, err
-	}
-	inputFile := fmt.Sprintf("%s/scripts/football/input.csv:/app/input.csv", cwd)
-	outputDir := fmt.Sprintf("%s/scripts/football/output:/app/Prints", cwd)
-
-	stdoutBytes := stdout.Bytes()
-	stderrStr := stderr.String()
+	inputFile, outputDir, err := buildVolumes(result.JobID)
 
 	cmd := exec.Command("docker", "run", "--rm", "-v", inputFile, "-v", outputDir, "football-rpa")
 	cmd.Stderr = &stderr
 	cmd.Stdout = &stdout
 	err = cmd.Run()
+
 	result.EndedAt = time.Now()
 	result.Job.FinishedAt = result.EndedAt
 	result.Task.FinishedAt = result.EndedAt
-	result.Payload = stdoutBytes
-	result.Logs = stderrStr
+	result.Payload = stdout.Bytes()
+	result.Logs = stderr.String()
+
+	if err != nil {
+		result.ExitCode = 1
+
+		// Se o processo retornou exit code != 0
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			result.ExitCode = exitErr.ExitCode()
+		}
+		return result, fmt.Errorf(
+			"erro ao executar football-rpa (exit code %d): %w\nstderr: %s",
+			result.ExitCode,
+			err,
+			result.Logs,
+		)
+	}
+
 	result.ExitCode = 0
 	if err := f.DB.FinishJob(ctx, result.Job); err != nil {
 		return ExecutionResult{}, err
 	}
 	return result, nil
+}
+
+func buildVolumes(jobID string) (string, string, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", "", err
+	}
+
+	inputFile := fmt.Sprintf("%s/scripts/football/input-%s.csv:/app/input-%s.csv", cwd, jobID, jobID)
+	outputDir := fmt.Sprintf("%s/scripts/football/output-%s:/app/Prints-%s", cwd, jobID, jobID)
+
+	return inputFile, outputDir, nil
 }
