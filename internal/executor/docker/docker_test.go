390f878263c9a9b827de816f8ea4fd6926119d48
Gustavo da Silva Oliveira <silva.gustavo98@gmail.com>
2026-08-07 19:54:20 -0300

    fix: update variable naming for clarity in Docker test cases

--- internal/executor/docker/docker_test.go
+++ internal/executor/docker/docker_test.go
@@ -8,13 +8,23 @@ import (
 
 func TestExecute(t *testing.T) {
 	t.Run("stderr e stdout ok sem erros", func(t *testing.T) {
-		docker := Docker{}
-		_, stdout, err := docker.Execute(context.Background(), "echo", []string{"hello"})
+		d := Docker{}
+		stdout, _, err := d.Execute(context.Background(), "echo", []string{"hello"})
 		if err != nil {
 			t.Fatalf("Erro ao executar echo: %v", err)
 		}
 		if strings.TrimSpace(string(stdout)) != "hello" {
 			t.Fatalf("Valor esperado: hello - Valor Recebido: %v", stdout)
+		}
+	})
+	t.Run("stderr com erros", func(t *testing.T) {
+		d := Docker{}
+		_, stderr, err := d.Execute(context.Background(), "bash", []string{"-c", "echo erro >&2; exit 42"})
+		if err == nil {
+			t.Error("Falha ao gerar erro no teste")
+		}
+		if strings.TrimSpace(string(stderr)) != "erro" {
+			t.Errorf("Falha ao ler stderr. Esperado: erro - Recebido: %s", stderr)
 		}
 	})
 }
